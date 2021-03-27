09e38a60768ae3589546e667427253eb5228a32c
Michael Stapelberg <michael@stapelberg.de>
2021-03-27 11:45:34 +0100

    console confusion

--- keyboards/kinesis/kint41/rules.mk
+++ keyboards/kinesis/kint41/rules.mk
@@ -6,4 +6,7 @@ BOARD = PJRC_TEENSY_4_1
 MCU  = cortex-m4
 ARMV = 7
 
-CONSOLE_ENABLE = yes        # Console for debug
+# TODO: this seems to have no effect here. putting it in stapelberg/rules.mk works instead.
+# # Console for debug
+# CONSOLE_ENABLE = yes
+
---
+++ keyboards/kinesis/stapelberg/rules.mk
@@ -0,0 +1,3 @@
+# Console for debug
+# TODO: results in SIGSEGV because of un-initialized chibios channel
+#CONSOLE_ENABLE = yes
