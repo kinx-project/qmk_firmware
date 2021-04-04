4b8ccf2d5e886a60ce47c8f987efcb2329f00fd7
Michael Stapelberg <michael@stapelberg.de>
2021-04-04 17:23:02 +0200

    kint41: use sym_eager_pk debounce algorithm
    see also https://beta.docs.qmk.fm/using-qmk/software-features/feature_debounce_type

--- keyboards/kinesis/kint41/rules.mk
+++ keyboards/kinesis/kint41/rules.mk
@@ -10,3 +10,6 @@ ARMV = 7
 # # Console for debug
 # CONSOLE_ENABLE = yes
 
+# Debounce eagerly (report change immediately), keep per-key timers. We can use
+# this because the kinT does not have to deal with noise.
+DEBOUNCE_TYPE = sym_eager_pk
